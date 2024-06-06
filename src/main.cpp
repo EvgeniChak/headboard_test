82577b976876cd130b520f695c4867e2353e0ca8
chak2 <chak@efcom.co.il>
2024-06-06 12:46:33 +0300

    init

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <EncButton.h>
+
+Button lowerButton = 2;
+Button upperButton = 3;
+
+void setup() {
+    Serial.begin(9600);
+    upperButton.setBtnLevel(HIGH);
+    upperButton.setClickTimeout(500);
+
+    lowerButton.setBtnLevel(HIGH);
+    lowerButton.setClickTimeout(500);
+}
+
+void loop() {
+    lowerButton.tick();
+    upperButton.tick();
+
+    if (upperButton.press() || upperButton.click() || upperButton.holding())
+        Serial.println("Upper button pressed");
+
+    if (lowerButton.press() || lowerButton.click() || lowerButton.holding())
+        Serial.println("Lower button pressed");
+}
